4f4dacaf6fb630f333fc79593c0370fc2cd57643
thisum <thisum.kankanamge@moht.com.sg>
2021-06-06 21:16:27 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p> Hi There! Keep changing</p>
+	<p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
